71343cbf746f1eedc9cb3ad9477120c7715ea368
hacker ali 17 <64998647+hacker-ali-17@users.noreply.github.com>
2021-08-21 21:39:38 +0100

    Update meta.sh

--- meta.sh
+++ meta.sh
@@ -1,3 +1,25 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────────────────────────────────────"$i"["$bi"•"$i"  "
+echo $i" |"$cy"                        Virus Palestinian Algerian "$i""
+echo $i" |"$me"──────────────────────────────────────────────────────────────────────────────────────"$i"            "
+echo $i" |"$pu" AUTHOR :"$ku" HACKER ALI 17  [Algeria]"$i"          "
+echo $i" |"$pu" FACEBOOK :"$ku" https://www.facebook.com/profile.php?id=100010795364272"                      $i"    "
+echo $i" |"$pu" the organization:"$cy"المنظمة المنتقمون لفلسطين "                                                        $i       "                                                             "
+echo $i" |"$pu" Telephone number:"$ku" 0664683258"$i"                                                               "
+echo $i" |"$pu"youtube :"$ku"https://www.youtube.com/channel/UCmEkXHi9eq0jU_tBT3BfBeQ?view_as=subscriber"$i"
+ $i|"$pu" github :"$ku"https://github.com/hacker-ali-17                                                        "
+echo $i"["$bi"•"$i"]"$me"─────────────────────────────────────────────────────────────────────────────"$i"["$bi"•"$i""
+
 apt update && apt upgrade
 apt install python2
 pip2 install urllib3 chardet certifi idna requests
@@ -20,3 +42,4 @@ git clone https://github.com/rapid7/metasploit-framework
 cd metasploit-framework
 bundle update nokogiri
 
+fi
